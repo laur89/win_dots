aa0de8bf42dc48bf1b455ad1a0611e8e30a37d97
Laur Aliste <laur.aliste@williamhill.com>
2018-10-03 05:26:41 +0200

    add misc/intellij/run_bash.bat

---
+++ misc/intellij/run_bash.bat
@@ -0,0 +1,12 @@
+REM from: https://gist.github.com/mpicker0/a6a3f10e6b9278074f93
+:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+set INTELLIJ=true
+::set TERM=vt100
+set PATH="C:\tools\cygwin\bin";%PATH%
+
+bash --login -i
+
+REM This works in IntelliJ 16
+REM Change the shell path in IntelliJ's Tools > Terminal to:
+REM C:\path\to\this\script\run_bash.bat
