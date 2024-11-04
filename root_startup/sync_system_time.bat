cb8371983529b7c49c1028b9af0d1587f433fae0
laur <laur.aliste@gmail.com>
2024-11-04 02:56:02 +0100

    run-sync-time: allow logging output to desktop
    - also add info in readme how to execute it as admin @ system startup;
    - also add exported Task Scheduler job xml executing it

--- root_startup/sync_system_time.bat
+++ root_startup/sync_system_time.bat
@@ -16,28 +16,43 @@ if not '%errorlevel%' == '0' (
 
 :: Change directory with passed argument. Processes started with
 :: "runas" start with forced C:\Windows\System32 workdir
-cd /d %1
+if "%~1"=="" (
+    echo No parameters have been provided.
+) else (
+    echo Parameters: %*
+    cd /d %1
+)
+
 
 :: Actual work:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 REM start time-sync service. this is especially useful if we've disabled most
 REM daemons via ChrisTitusTech/winutil (or other similar debloater) -- otherwise
 REM our system time is possibly off.
 :: following logic from https://stackoverflow.com/a/35626035 (or more exactly, from the linked gist @ https://gist.github.com/thedom85/dbeb58627adfb3d5c3af)
-@echo on & @setlocal enableextensions
-@echo =========================
-@echo Turn off the time service
-net stop w32time
-@echo ======================================================================
-@echo Set the SNTP (Simple Network Time Protocol) source for the time server
-w32tm /config /syncfromflags:manual /manualpeerlist:"0.it.pool.ntp.org 1.it.pool.ntp.org 2.it.pool.ntp.org 3.it.pool.ntp.org"
-@echo =============================================
-@echo ... and then turn on the time service back on
-net start w32time
-@echo =============================================
-@echo Tell the time sync service to use the changes
-w32tm /config /update
-@echo =======================================================
-@echo Reset the local computer's time against the time server
-w32tm /resync /rediscover
-::pause
+
+:: enable delayed expansion, so we can get the updated date-time
+(
+    @echo on & @setlocal enableextensions EnableDelayedExpansion
+    @echo start: user: %username% @ !date! !time!
+        
+    @echo =======================================================
+    @echo Turn off the time service...
+    net stop w32time
+    @echo =======================================================
+    @echo Set the SNTP source for the time server
+    w32tm /config /syncfromflags:manual /manualpeerlist:"0.it.pool.ntp.org 1.it.pool.ntp.org 2.it.pool.ntp.org 3.it.pool.ntp.org"
+    @echo =======================================================
+    @echo ...and then turn on the time service back on
+    net start w32time
+    @echo =======================================================
+    @echo Tell the time sync service to use the changes
+    w32tm /config /update
+    @echo =======================================================
+    @echo Reset the local computer's time against the time server
+    w32tm /resync /rediscover
+
+    @echo end: user: %username% @ !date! !time!
+    @echo -------------------------------------------------------------------------------------------
+rem )
+)  >> C:\Users\%username%\Desktop\sync_time_log.txt
 @endlocal & @goto :EOF
